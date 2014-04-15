22c9125866c8bff5d239eae9b8e4a95d85184706
Bryan Jensen <bryanawjensen@gmail.com>
2014-04-14 20:27:30 -0400

    Gousie is impossible

--- Projects/CPP_SPIM3.cpp
+++ Projects/CPP_SPIM3.cpp
@@ -2,48 +2,16 @@
 
 using namespace std;
 
-int fibRecur(int n, int* array) {
-	// cout << "Recursing with " << n << " and " << array[4] << endl;
-	if (array[n] == -1) {
-		array[n] = fibRecur(n-1, array) + fibRecur(n-2, array);
-	}
+int fibRecur(int n) {
 
-	return array[n];
 }
 
-int fib(int n, int* array) {
-	array[0] = 0;
-	array[1] = 1;
-
-	fibRecur(n, array);
-
-	int sum = 0;
-	for (int i = n; i > 0; i--) {
-		sum += array[i];
-
-		cout << i;
-
-		if (i < 10) {
-			cout << ":  ";
-		}
-		else {
-			cout << ": ";
-		}
-
-		cout << array[i] << endl;
-	}
-
-	return sum;
+int fib(int n) {
+	return fibRecur(n)
 }
 
 int main() {
-	int memoArray[45] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-						  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-						  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-						  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-						  -1, -1, -1, -1, -1 };
-
-	int ans = fib(44, memoArray);
+	int ans = fib(44);
 	cout << "The sum of the fibonacci numbers is " << ans << endl;
 
 	cout << "\nFinished, terminating." << endl;
